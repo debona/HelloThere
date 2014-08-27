9a2982c483ec8a347d4c3a6197def3cd35586a5f
Thomas DE BONA <thomas.debona@gmail.com>
2014-08-27 14:31:12 +0200

    Create a basic user model

---
+++ HelloThere/Models/_User.h
@@ -0,0 +1,18 @@
+//
+//  _User.h
+//  HelloThere
+//
+//  Created by Thomas DE BONA on 27/08/2014.
+//  Copyright (c) 2014 debona. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface _User : NSObject
+
+@property (strong, nonatomic) NSString *username;
+@property (strong, nonatomic) NSString *photo;
+@property (strong, nonatomic) NSString *fullname;
+@property (strong, nonatomic) UIColor *color;
+
+@end
---
+++ HelloThere/Models/_User.m
@@ -0,0 +1,18 @@
+//
+//  _User.m
+//  HelloThere
+//
+//  Created by Thomas DE BONA on 27/08/2014.
+//  Copyright (c) 2014 debona. All rights reserved.
+//
+
+#import "_User.h"
+
+@interface _User ()
+
+@end
+
+@implementation _User
+
+
+@end
