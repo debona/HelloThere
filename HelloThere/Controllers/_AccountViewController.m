cebf02b1a681a99f6f0a4665b49847fa88576ff6
Thomas DE BONA <thomas.debona@gmail.com>
2014-08-27 12:57:20 +0200

    Setup a basic account screen layout

--- HelloThere/Controllers/_AccountViewController.m
+++ HelloThere/Controllers/_AccountViewController.m
@@ -8,11 +8,21 @@
 
 #import "_AccountViewController.h"
 
+#import "UIView+endEditingOnTap.h"
+
 #import "IonIcons.h"
 
-@interface _AccountViewController ()
+@interface _AccountViewController () <UITableViewDataSource>
 
-@property (weak, nonatomic) IBOutlet UIImageView *image;
+@property (weak, nonatomic) IBOutlet UIImageView *photo;
+
+@property (weak, nonatomic) IBOutlet UITableViewCell *usernameCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *fullnameCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *colorCell;
+
+@property (weak, nonatomic) IBOutlet UITextField *colorTeaser;
+
+@property (strong, nonatomic) NSArray *cells;
 
 @end
 
@@ -28,12 +38,41 @@
                                              iconSize:46
                                             imageSize:CGSizeMake(40, 40)];
     
+    self.photo.image = [IonIcons imageWithIcon:icon_ios7_person_outline
+                                          iconColor:[UIColor whiteColor]
+                                           iconSize:150
+                                          imageSize:self.photo.frame.size];
+    self.cells = @[
+                   self.usernameCell,
+                   self.fullnameCell,
+                   self.colorCell,
+                   ];
+
+    [self.view endEditingOnTap];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [self.cells count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [self.cells objectAtIndex:indexPath.row];
 }
 
 @end
